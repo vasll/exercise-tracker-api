3acdb076a90f745a244d8a9597214764a5746747
vasll <valentinosoll17@gmail.com>
2023-09-16 12:12:51 +0200

    Added route logic

--- routes/api.js
+++ routes/api.js
@@ -2,28 +2,60 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
+const Exercise = require('../models/exercise')
 
 // Creates a new user
 router.post("/users", async (req, res) => {
+    // Username validation
     const username = req.body.username;
-    // TODO validation
+    if (!isValidUsername(username)) {
+        return res.status(400).json({ error: 'Invalid username' });
+    }
 
-    // TODO check for duplicate usernames
-
-    const dbUser = await (new User({username: username}).save());
-    // TODO try catch
-
-    return res.json({username: dbUser.username, _id: dbUser._id})
+    // Check if username is already in db, if it does return that user instance, otherwise make a new one
+    try{
+        var user = await User.findOne({username: username});
+        if (user == null) {
+            user = await new User({username: username}).save()
+        };
+        return res.json({username: user.username, _id: user._id});
+    } catch(err) {
+        return res.status(500).json({error: 'Server error'});
+    }
 })
 
 // Returns all the users in an array
 router.get("/users", async (req, res) => {
-    // TODO
+    try{
+        const users = await User.find().select('-__v');  // Exclude the __v field
+        return res.json(users);
+    } catch (err) {
+        return res.status(500).json({error: 'Server error'});
+    }
 })
 
 // Adds an exercise based on a user id
 router.post("/users/:_id/exercises", async (req, res) => {
-    // TODO
+    const {description, duration, date} = req.body;
+    const userId = req.params._id;
+
+    // TODO validate duration
+    // TODO validate date
+
+    try {
+        // Check if user with given _id exists in database
+        const user = await User.findOne({_id: userId});
+        if (user == null) {
+            return res.status(500).json({error: 'User not found'})
+        };
+
+        const exercise = await new Exercise({
+            username: user._id, description, duration, date
+        }).save();
+        return res.json(exercise);
+    } catch (err) {
+        return res.status(500).json({ error: 'Server error' });
+    }
 })
 
 // Returns a user object with a count of the exercises added, 
@@ -31,5 +63,11 @@ router.post("/users/:_id/exercises", async (req, res) => {
 router.get("/users/:_id/logs", async (req, res) => {
     // TODO
 })
+
+// Functions
+function isValidUsername(username) {
+    const regex = /^[a-zA-Z0-9_]{3,20}$/; // Alphanumeric and underscore, 3-20 characters
+    return regex.test(username);
+}
 
 module.exports = router;
